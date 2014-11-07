80f4baf6bd34083a1a3188c9d88b6755e081f2f5
Andrew Stuart <andrew.stuart2@gmail.com>
2014-11-07 16:23:35 -0700

    Update restlet search.

--- restlet.js
+++ restlet.js
@@ -1,15 +1,15 @@
+//The restlet that will need to be uploaded to NetSuite for the gulp plugin to run.
+
 function writeFile (data) {
-  var files = nlapiSearchRecord('file', null, new nlobjSearchFilter('name', null, 'is', data.name), new nlobjSearchColumn('folder'));
+  var files = nlapiSearchRecord('file', null, [new nlobjSearchFilter('name', null, 'is', data.name)], [new nlobjSearchColumn('folder')]);
 
   var newFileNumber;
-
   if(files.length === 1) {
     var file = nlapiCreateFile(data.name, 'PLAINTEXT', data.content);
-    file.setFolder(files[0].getFieldValue('folder'));
+    file.setFolder(files[0].getValue('folder'));
 
     newFileNumber = nlapiSubmitFile(file);
   }
 
-  tc.debug('newFileNumber', newFileNumber);
   return newFileNumber;
 }
