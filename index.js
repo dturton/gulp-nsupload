96282d9e81782e93f2da6c8ec377d4297ff7da7d
Andrew Stuart <andrew.stuart2@gmail.com>
2014-12-12 15:32:24 -0700

    Move upload functionality to separate reusable module.

--- index.js
+++ index.js
@@ -1,62 +1,17 @@
-var request = require('request')
-  , _ = require('underscore')
-  , path = require('path')
+var nsupload = require('nsupload')
   , through = require('through');
-
-var URL_TEMPLATE = 'https://<%= domain %>/app/site/hosting/restlet.nl';
-var url;
-
-var AUTH_STRING = 'NLAuth  nlauth_account=<%= account %>, nlauth_email=<%= email %>, nlauth_signature=<%= password %>';
-
-//Catch files
-function sendFile (file) {
-  var authHeader = _.template(AUTH_STRING)(config);
-
-  //Upload them to netsuite by name
-  var fileName = path.basename(file.path);
-
-  if(file._contents) {
-
-    //Pipe the response
-    request({
-      uri: url,
-      qs: {
-        deploy: 1,
-        script: config.script
-      },
-      method: 'PUT',
-      headers: {
-        Authorization: authHeader
-      },
-      json: {
-        name: fileName,
-        path: file.path,
-        content: file._contents.toString()
-      }
-    }, function(err, res, data) {
-      if(err || !res) {
-        console.log('Error uploading file ' + file.path);
-        console.log(err);
-      } else if(res.body && res.body.statusCode && res.body.statusCode !== 200){
-        console.log('Error');
-        console.log(res.statusCode);
-      } else {
-        console.log('Successfully uploaded file ' + file.path);
-      }
-    });
-  }
-}
 
 module.exports = function(options) {
   options.domain = options.domain || 'rest.sandbox.netsuite.com';
 
-  url = _.template(URL_TEMPLATE)(options);
-  if(!(options && options.email && options.password && options.account && options.script)) {
-    throw new Error('Options are required. Please provide {email:\'\', password:\'\', account: 123, script:123}');
-  }
-
-  config = options;
-  return through(sendFile);
+  nsupload.config(options);
+  return through(function(file) {
+    if(file.path) {
+      nsupload(file.path).then(function() {
+        console.log(file.path + ' uploaded.');
+      }, function(e) {
+        console.log('Error', e);
+      });
+    }
+  });
 };
-
-module.exports.send = sendFile;
